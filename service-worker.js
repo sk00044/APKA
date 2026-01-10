6f41d2e07912019911058575e50e223b5a23c50a
sk00044 <pokeassclub@gmail.com>
2026-01-10 12:29:33 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,50 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("apka-cache").then((cache) =>
-      cache.addAll([
-        "/APKA/",
-        "/APKA/index.html",
-        "/APKA/style.css",
-        "/APKA/script.js"
-      ])
+const CACHE_NAME = "apka-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/APKA/",
+  "/APKA/index.html",
+  "/APKA/style.css",
+  "/APKA/script.js",
+
+  // icons & images
+  "/APKA/icon.png",
+  "/APKA/raymond.png",
+  "/APKA/siyarams.jpg",
+  "/APKA/dj.png",
+  "/APKA/arvind.png"
+];
+
+// INSTALL
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// ACTIVATE (old cache clean)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
     )
   );
+  self.clients.claim();
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+
+// FETCH
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
